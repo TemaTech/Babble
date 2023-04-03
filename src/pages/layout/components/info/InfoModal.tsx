6a9ddf426ec7eff0767f1b347104fad2705e1589
cherartem <artem138927@gmail.com>
2023-04-03 18:55:04 +0200

    Add Info modal to the dropdown menu in the navbar

---
+++ src/pages/layout/components/info/InfoModal.tsx
@@ -0,0 +1,47 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  MenuItem,
+  Text,
+  Highlight,
+  Link,
+} from '@chakra-ui/react'
+import { InfoOutlineIcon } from "@chakra-ui/icons"
+
+export const InfoModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <MenuItem icon={<InfoOutlineIcon />} onClick={onOpen}>Info</MenuItem>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Information about Babble</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text lineHeight='tall'>
+                "Babble" is a chatting app, but also, it is intended to be a project that showcases the skills I've acquired during 
+                the JavaScript Course of The Odin Project's curriculum. It was a quite interesting 
+                experience building this kind of application where the user could register or log in, 
+                set up his profile name and picture, create a private chat, create a group chat and 
+                send a message. So, as this project is considered more as a portfolio project, here 
+                are the technologies I've used in this app: React, TypeScript, ChakraUI, React-Router, Jotai, 
+                Jest, React-Testing-Library and Firebase.
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Link textDecoration='underline' color='blue.500' href="https://twitter.com/cherrartem" isExternal mr='3'>My Twitter</Link> 
+            <Link textDecoration='underline' color='blue.500' href="https://github.com/TemaTech" isExternal>My GitHub</Link> 
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
--- src/pages/layout/components/sidebar/navbar/DropdownMenu.tsx
+++ src/pages/layout/components/sidebar/navbar/DropdownMenu.tsx
@@ -1,11 +1,21 @@
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import { Button, IconButton } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { InfoOutlineIcon } from "@chakra-ui/icons"
 import { MdOutlineExitToApp } from 'react-icons/md'
 import { ProfileModal } from '../../profile/ProfileModal'
+import { signOut } from "@firebase/auth";
+import { auth } from "../../../../../firebase/config";
+import { InfoModal } from '../../info/InfoModal'
 
 export const DropdownMenu = () => {
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error("Error while signing out: ", err);
+    }
+  }
+
   return (
     <Menu>
       <MenuButton
@@ -16,8 +26,8 @@ export const DropdownMenu = () => {
       />
       <MenuList>
         <ProfileModal />
-        <MenuItem icon={<InfoOutlineIcon />}>Info</MenuItem>
-        <MenuItem icon={<MdOutlineExitToApp />}>Log Out</MenuItem>
+        <InfoModal />
+        <MenuItem icon={<MdOutlineExitToApp />} onClick={handleSignOut}>Log Out</MenuItem>
       </MenuList>
     </Menu>
   );
